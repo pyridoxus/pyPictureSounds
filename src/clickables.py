819cdbb8a5acef4d17deb10e0d729951121e0663
root <root@phoenix.pyridxs>
2011-04-10 13:49:24 -0500

    Added gitignore. Other minor changes.

--- src/clickables.py
+++ src/clickables.py
@@ -29,12 +29,12 @@ class ClickableObject(pygame.sprite.Sprite):
         '''
         Draw the image.
         '''
-        self.__x += self.__dir
-        self.__y += self.__dir
-        self.__rect.move(self.__x, self.__y)
+        self.__x += self.__dir * 2
+        self.__y += self.__dir * 2
         temp = pygame.transform.smoothscale(self.__image,
                                             (self.__size[0] - self.__y,
                                              self.__size[1] - self.__y))
+        self.__rect.move_ip(self.__dir, self.__dir)
         if self.__y >= self.__size[1]:
             self.__dir = -1
         if self.__y <= 0:
