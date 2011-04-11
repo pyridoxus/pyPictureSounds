8f62af77014088244b0a19801f02f17ffbfd115e
root <root@phoenix.pyridxs>
2011-04-11 05:57:44 -0500

    Added all images and sounds. Added more work to begin moving the objects on screen.

--- src/clickables.py
+++ src/clickables.py
@@ -16,13 +16,48 @@ class ClickableObject(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.__image = pygame.image.load(imageFile)
         self.__sound = pygame.mixer.Sound(soundFile)
-        self.__sound.play()
+#        self.__sound.play()
         self.__rect = self.__image.get_rect()
         self.__x = 0
         self.__y = 0
+        self.__row = None
         self.__dir = 1
         self.__size = size
-        self.__image = pygame.transform.smoothscale(self.__image, self.__size)
+        self.__icon = pygame.transform.smoothscale(self.__image,
+                                            (self.__size[0],
+                                             self.__size[1]))
+        
+        
+    def setRow(self, row):
+        '''
+        Set the row position for this object.
+        '''
+        self.__row = row
+        
+        
+    def setXY(self, x, y):
+        '''
+        Set the x,y coordinates for this object.
+        '''
+        self.__x = x
+        self.__y = y
+        self.__icon.get_rect().topleft = x, y
+        
+        
+    def setDir(self, d):
+        '''
+        Set direction of image movement.
+        '''
+        self.__dir = d
+        
+                
+    def drawIcon(self, bmp):
+        '''
+        Draw the iconic version of the image at the object's coordinates.
+        '''
+        temp = self.__icon.get_rect()
+        temp.move_ip(self.__dir, 0)
+        bmp.blit(self.__icon, temp)
         
         
     def draw(self, bmp):
--- src/pictureSounds.py
+++ src/pictureSounds.py
@@ -6,41 +6,88 @@ Created on Mar 20, 2011
 
 import sys, pygame
 from clickables import ClickableObject
+from objectList import objectList
 
-pygame.init()
-size = width, height = 640,480
-speed = [2, 2]
-black = 0, 0, 0
-screen = pygame.display.set_mode(size)
-ball = pygame.image.load("/home/pyridoxus/workspace/pyPictureSounds/"
-                         "images/ball.gif")
-ballrect = ball.get_rect()
-#while 1:
-#    for event in pygame.event.get():
-#        if event.type == pygame.QUIT:
-#            sys.exit()
-#    ballrect = ballrect.move(speed)
-#    if ballrect.left < 0 or ballrect.right > width:
-#        speed[0] = -speed[0]
-#    if ballrect.top < 0 or ballrect.bottom > height:
-#        speed[1] = -speed[1]
-#    screen.fill(black)
-#    screen.blit(ball, ballrect)
-#    pygame.display.flip()
-
-def loop():
-    allSprites = pygame.sprite.RenderUpdates()
-    ClickableObject.containers = allSprites
-    clicker = ClickableObject("/home/pyridoxus/workspace/"
-                              "pyPictureSounds/images/frog.bmp",
-                              "/home/pyridoxus/workspace/"
-                              "pyPictureSounds/sounds/FROG.WAV", size)
-    while(True):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(black)
-        clicker.draw(screen)
-        pygame.display.flip()
+class Game(object):
+    '''
+    Main game object.
+    '''
+    def __init__(self):
+        '''
+        Setup the game.
+        '''
+        pygame.init()
+        self.__size = 640,480
+        self.__speed = [2, 2]
+        self.__black = 0, 0, 0
+        self.__screen = pygame.display.set_mode(self.__size)
+        self.__objects = []
+        self.__objectSize = 128, 128
+        self.__objectSpacing = 16
+        self.__initObjects()
+        self.__xBase = 0
+        self.__yBase = 0
+        self.__xEnd = 0
+        self.initPositions()
         
-loop()
+        
+    def __initObjects(self):
+        '''
+        Init the object list.
+        '''
+        allSprites = pygame.sprite.RenderUpdates()
+        ClickableObject.containers = allSprites
+        imageDirectory = "/home/pyridoxus/workspace/pyPictureSounds/images/"
+        soundDirectory = "/home/pyridoxus/workspace/pyPictureSounds/sounds/"
+
+        for image, sound in objectList:
+            clicker = ClickableObject("%s%s" % (imageDirectory, image),
+                                      "%s%s" % (soundDirectory, sound),
+                                      self.__objectSize)
+            self.__objects.append(clicker)
+        
+        
+    def initPositions(self):
+        '''
+        Setup the object's positions
+        '''
+#    int rows, columns, index, x, y;
+        index = 0
+        rows = self.__size[1] / (self.__objectSize[1] + self.__objectSpacing)
+        columns = len(self.__objects) / rows + 1
+        self.__xBase = self.__size[0] / 2 - \
+            (((self.__objectSize[0] + self.__objectSpacing) * columns) - \
+             self.__objectSpacing) / 2
+        self.__yBase = self.__size[1] / 2 - \
+                (((self.__objectSize[1] + self.__objectSpacing) * rows) - \
+                 self.__objectSpacing) / 2
+        self.__xEnd = self.__xBase
+        for b in range(rows):
+            y = self.__yBase + (self.__objectSize[1] + self.__objectSpacing) * b
+            for a in range(columns):
+                self.__objects[index].setRow(b)
+                self.__objects[index].setDir(b % 2)
+                x = self.__xBase + a * (self.__objectSize[0] + \
+                                        self.__objectSpacing)
+                self.__objects[index].setXY(x, y)
+                index += 1
+                if index == len(self.__objects):
+                    b = rows
+                    break
+        if x > self.__xEnd:
+            self.__xEnd = x + self.__objectSize[0] + self.__objectSpacing
+#    }
+#    return;
+        
+    def loop(self):
+        while(True):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            self.__screen.fill(self.__black)
+            for ob in self.__objects:
+                ob.drawIcon(self.__screen)
+            pygame.display.flip()
+        
+game = Game()
+game.loop()
