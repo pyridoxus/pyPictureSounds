f461ee4dd6224f367517feb01fe2cf54f9735c40
root <root@phoenix.pyridxs>
2011-04-16 14:30:18 -0500

    Added interface object to hold onto data between game loop and icons.

--- src/clickables.py
+++ src/clickables.py
@@ -5,11 +5,68 @@ Created on Mar 27, 2011
 '''
 import pygame
 
+class Interface(object):
+    '''
+    Interface between the ClickableObjects and the game loop.
+    '''
+    def __init__(self, left, right, size, spacing):
+        '''
+        Setup internals.
+        '''
+        self.__left = left
+        self.__right = right
+        self.__size = size
+        self.__spacing = spacing
+        
+        
+    def setLeft(self, x):
+        '''
+        Set the left-most coordinate.
+        '''
+        self.__left = x
+        
+        
+    def getLeft(self):
+        '''
+        Return the left-most coordinate.
+        '''
+        return self.__left
+    
+    
+    def setRight(self, x):
+        '''
+        Set the right-most coordinate.
+        '''
+        self.__right = x
+        
+        
+    def getRight(self):
+        '''
+        Return the right-most coordinate.
+        '''
+        return self.__right
+    
+    
+    def getSize(self):
+        '''
+        Return the size of the icons.
+        '''
+        return self.__size
+    
+    
+    def getSpacing(self):
+        '''
+        Return the spacing size between icons.
+        '''
+        return self.__spacing
+    
+    
+
 class ClickableObject(pygame.sprite.Sprite):
     '''
     This is a clickable image with associated sound effect.
     '''
-    def __init__(self, imageFile, soundFile, size):
+    def __init__(self, imageFile, soundFile, interface):
         '''
         Set up the object with the image and the sound.
         '''
@@ -17,15 +74,15 @@ class ClickableObject(pygame.sprite.Sprite):
         self.__image = pygame.image.load(imageFile)
         self.__sound = pygame.mixer.Sound(soundFile)
 #        self.__sound.play()
-        self.__rect = self.__image.get_rect()
         self.__x = 0
         self.__y = 0
         self.__row = None
         self.__dir = 1
-        self.__size = size
+        self.__interface = interface
         self.__icon = pygame.transform.smoothscale(self.__image,
-                                            (self.__size[0],
-                                             self.__size[1]))
+                                            (self.__interface.getSize()[0],
+                                             self.__interface.getSize()[1]))
+        self.__rect = self.__icon.get_rect()
         
         
     def setRow(self, row):
@@ -41,7 +98,7 @@ class ClickableObject(pygame.sprite.Sprite):
         '''
         self.__x = x
         self.__y = y
-        self.__icon.get_rect().topleft = x, y
+        self.__rect = self.__rect.move(x, y)
         
         
     def setDir(self, d):
@@ -55,9 +112,20 @@ class ClickableObject(pygame.sprite.Sprite):
         '''
         Draw the iconic version of the image at the object's coordinates.
         '''
-        temp = self.__icon.get_rect()
-        temp.move_ip(self.__dir, 0)
-        bmp.blit(self.__icon, temp)
+        self.__x += self.__dir
+        if self.__x < self.__interface.getLeft() and self.__dir == -1:
+            self.__x = self.__interface.getRight() + \
+                        self.__interface.getSize()[0] + \
+                        self.__interface.getSpacing() 
+            self.__rect = self.__icon.get_rect()
+            self.__rect = self.__rect.move(self.__x, self.__y)
+        elif self.__x > self.__interface.getRight() and self.__dir == 1:
+            self.__rect = self.__icon.get_rect()
+            self.__x = self.__interface.getLeft()
+            self.__rect = self.__rect.move(self.__x, self.__y)
+        else:
+            self.__rect = self.__rect.move(self.__dir, 0)
+        bmp.blit(self.__icon, self.__rect)
         
         
     def draw(self, bmp):
@@ -67,10 +135,12 @@ class ClickableObject(pygame.sprite.Sprite):
         self.__x += self.__dir * 2
         self.__y += self.__dir * 2
         temp = pygame.transform.smoothscale(self.__image,
-                                            (self.__size[0] - self.__y,
-                                             self.__size[1] - self.__y))
+                                            (self.__interface.getSize()[0] -\
+                                             self.__y,
+                                             self.__interface.getSize()[1] -\
+                                             self.__y))
         self.__rect.move_ip(self.__dir, self.__dir)
-        if self.__y >= self.__size[1]:
+        if self.__y >= self.__interface.getSize()[1]:
             self.__dir = -1
         if self.__y <= 0:
             self.__dir = 1
--- src/pictureSounds.py
+++ src/pictureSounds.py
@@ -5,8 +5,9 @@ Created on Mar 20, 2011
 '''
 
 import sys, pygame
-from clickables import ClickableObject
+from clickables import (ClickableObject, Interface)
 from objectList import objectList
+from time import sleep
 
 class Game(object):
     '''
@@ -22,12 +23,9 @@ class Game(object):
         self.__black = 0, 0, 0
         self.__screen = pygame.display.set_mode(self.__size)
         self.__objects = []
-        self.__objectSize = 128, 128
-        self.__objectSpacing = 16
+        self.__interface = Interface(0, 0, [128, 128], 16)
         self.__initObjects()
-        self.__xBase = 0
         self.__yBase = 0
-        self.__xEnd = 0
         self.initPositions()
         
         
@@ -43,8 +41,9 @@ class Game(object):
         for image, sound in objectList:
             clicker = ClickableObject("%s%s" % (imageDirectory, image),
                                       "%s%s" % (soundDirectory, sound),
-                                      self.__objectSize)
+                                      self.__interface)
             self.__objects.append(clicker)
+        print len(self.__objects)
         
         
     def initPositions(self):
@@ -53,29 +52,37 @@ class Game(object):
         '''
 #    int rows, columns, index, x, y;
         index = 0
-        rows = self.__size[1] / (self.__objectSize[1] + self.__objectSpacing)
+        spacing = self.__interface.getSpacing()
+        objectSize = self.__interface.getSize()
+        rows = self.__size[1] / (objectSize[1] + spacing)
         columns = len(self.__objects) / rows + 1
-        self.__xBase = self.__size[0] / 2 - \
-            (((self.__objectSize[0] + self.__objectSpacing) * columns) - \
-             self.__objectSpacing) / 2
+        xBase = self.__size[0] / 2 - \
+            (((objectSize[0] + spacing) * columns) - \
+             spacing) / 2
         self.__yBase = self.__size[1] / 2 - \
-                (((self.__objectSize[1] + self.__objectSpacing) * rows) - \
-                 self.__objectSpacing) / 2
-        self.__xEnd = self.__xBase
+                (((objectSize[1] + spacing) * rows) - \
+                 spacing) / 2
+        xEnd = xBase
         for b in range(rows):
-            y = self.__yBase + (self.__objectSize[1] + self.__objectSpacing) * b
+            y = self.__yBase + (objectSize[1] + spacing) * b
             for a in range(columns):
                 self.__objects[index].setRow(b)
-                self.__objects[index].setDir(b % 2)
-                x = self.__xBase + a * (self.__objectSize[0] + \
-                                        self.__objectSpacing)
+                d = [-1, 1][b % 2]
+                self.__objects[index].setDir(d)
+                x = xBase + a * (objectSize[0] + \
+                                        spacing)
                 self.__objects[index].setXY(x, y)
+                print "(%d, %d)" % (x, y)
                 index += 1
                 if index == len(self.__objects):
                     b = rows
                     break
-        if x > self.__xEnd:
-            self.__xEnd = x + self.__objectSize[0] + self.__objectSpacing
+        if x > xEnd:
+            xEnd = x + objectSize[0] + spacing
+            
+        self.__interface.setLeft(xBase)
+        self.__interface.setRight(xEnd)
+            
 #    }
 #    return;
         
